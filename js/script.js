bea43a60643205661de29e4520ccc0d970e01eed
Oswaldo Alonzo <oalonzo@gmail.com>
2021-07-04 20:08:42 -0600

    configuración para obtener las imagenes de los pokemons de la api.

--- js/script.js
+++ js/script.js
@@ -2,13 +2,55 @@ const d = document,
   $main = d.querySelector("main"),
   $links = d.querySelector(".links");
 
-let pokeAPI = "https://pokeapi.co/api/v2/pokemon/"
+let pokeAPI = "https://pokeapi.co/api/v2/pokemon/";
 
 async function loadPokemons(url) {
-    try {
+  try {
+    $main.innerHTML = `<img class="loader" src="/pokemon_fetch/assets/img/bars.svg" alt="Cargando..."  />`;
+    let res = await fetch(url),
+      json = await res.json(),
+      $template = "",
+      $prevLink,
+      $nextLink;
+    //console.log(json);
 
-    } catch (err) {
+    //caputrando error
+    if (!res.ok) throw { status: res.status, statusText: res.statusText };
 
+    for (let i = 0; i < json.results.length; i++) {
+      //console.log(json.results[i]);
+      try {
+        let res = await fetch(json.results[i].url),
+          pokemon = await res.json();
+
+        console.log(res, pokemon);
+
+        if (!res.ok) throw { status: res.status, statusText: res.statusText };
+        //Generando el Template HTML
+        $template += `
+        <figure>
+        <img src="${pokemon.sprites.front_default}" alt="${pokemon.name}">
+        <figcaption>${pokemon.name}</figcaption>
+        </figure>
+        `;
+
+        //Capturando Error
+      } catch (err) {
+        console.log(err);
+        let message = err.statusText || "Ocurió un Error";
+        $templeate += `
+        <figure>
+        <figcaption>Error ${err.status}: ${message}</figcaption>
+        </figure>`;
+      }
     }
+
+    $main.innerHTML = $template;
+  } catch (err) {
+    //console.log(err);
+    let message = err.statusText || "Ocurió un Error";
+    $main.innerHTML = `<p>Error ${err.status}: ${message}</p>`;
+  }
 }
-  d.addEventListener("DOMContentLoad, e=> loadPokemons('pokeAPI'));
+
+loadPokemons(pokeAPI);
