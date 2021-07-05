6da3ef0d7da11f7759cf58ef0d6a49e0c5cd5b27
Oswaldo Alonzo <oalonzo@gmail.com>
2021-07-04 19:07:02 -0600

    Agregando los archivos para la conexion a Pokemon

---
+++ js/script.js
@@ -0,0 +1,14 @@
+const d = document,
+  $main = d.querySelector("main"),
+  $links = d.querySelector(".links");
+
+let pokeAPI = "https://pokeapi.co/api/v2/pokemon/"
+
+async function loadPokemons(url) {
+    try {
+
+    } catch (err) {
+
+    }
+}
+  d.addEventListener("DOMContentLoad, e=> loadPokemons('pokeAPI'));
